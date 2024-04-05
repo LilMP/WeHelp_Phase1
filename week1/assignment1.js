5828c46be193093665b1ca1fbe6581a65703a9c8
LilMP <m10809211@gapps.ntust.edu.tw>
2024-04-05 12:05:56 +0800

    js: 針對 panel 顯示的dom操作

--- week1/assignment1.js
+++ week1/assignment1.js
@@ -1,50 +1,50 @@
 
 
-console.log("hello world");
+// console.log("hello world");
 
 function ExpendPanelToggle(params) {
     console.log("expand panel");
+    // getElementById
     // document.getElementById("hamburger").style.backgroundColor = "red";
-    // console.log(document.getElementsByClassName("navbar_right"));
+    
+    // getElementsByClassName: 注意回傳的是 array
     // document.getElementsByClassName("navbar_right").item(0).style.backgroundColor = "green";
-    // document.getElementsByClassName("navbar_right").item(0).style.height = "100%";
-    // document.getElementsByClassName("panel").item(0).style.border = "1px solid red";
+   
 
     // document.getElementsByClassName("panel").item(0).style.position = "fixed";
-    // document.getElementsByClassName("panel").item(0).style.display = "block";
-    // document.getElementsByClassName("panel").item(0).style.display = "block";
-    // document.getElementsByClassName("panel").item(0).style.display = "block";
     // document.getElementsByClassName("navbar_item").style.display = "inherit";
 
-    // var items = document.getElementsByClassName('navbar_item');
-    
-    // if(items[0].style.display == "none"){
-    //     for (var i = 0; i < items.length; i++) {
-    //         // console.log(items[i]);
-    //         items[i].style.display = "block";
-    //     }
-    // }else{
-    //     for (var i = 0; i < items.length; i++) {
-    //         // console.log(items[i]);
-    //         items[i].style.display = "none";
-    //     }
-    // }
-    
-    // document.getElementById("hamburger").style.position = "relative";
-    
-    var panel_status = document.getElementsByClassName("panel").item(0).style.display;
-    if(panel_status = "none"){
+    var items = document.getElementsByClassName('navbar_item');
+    // console.log(items[0].style.display);
+    if(items[0].style.display == ""){
+        for (var i = 0; i < items.length; i++) {
+            // console.log(items[i]);
+            items[i].style.display = "flex";
+        }
         document.getElementsByClassName("panel").item(0).style.display = "block";
     }else{
-        document.getElementsByClassName("panel").item(0).style.display = "none";
-
+        for (var i = 0; i < items.length; i++) {
+            // console.log(items[i]);
+            items[i].style.display = "";
+        }
+        document.getElementsByClassName("panel").item(0).style.display = "flex";
     }
+    
+    
 
 
     document.getElementsByClassName("navbar_right").item(0).classList.toggle('panel_expand');
 
-    document.getElementsByClassName("topright_btn").item(0).innerHTML = "<p onclick='ExpendPanelToggle()'>x</p>";
+    // console.log(document.getElementsByClassName("navbar_right").item(0));
+    if(document.getElementsByClassName("navbar_right").item(0).classList.contains("panel_expand")){
+        // console.log("yes");
+        document.getElementsByClassName("topright_btn").item(0).innerHTML = "<p onclick='ExpendPanelToggle()'>X</p>";
 
-    // <img id="hamburger" onclick="ExpendPanelToggle()" src="./img/hamburger.png" alt="hamburger"></img>
+    }else{
+        // console.log("no");
+        document.getElementsByClassName("topright_btn").item(0).innerHTML = '<img id="hamburger" onclick="ExpendPanelToggle()" src="./img/hamburger.png" alt="hamburger"></img>';
+
+
+    }
 
 }
