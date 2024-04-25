2be137117370b01dd53f7de7c5f0351dee20fcbc
LilMP <m10809211@gapps.ntust.edu.tw>
2024-04-26 00:27:37 +0800

    新增輸入空值檢查、error page可以根據輸入資料的情況給予不同錯誤訊息

--- week4/main.py
+++ week4/main.py
@@ -30,75 +30,69 @@ database = {
 
 @app.get('/')
 async def home_page(request: Request):
-    
-    # print("homepage: 印出request.session:")
-    # print(request.session.get("SIGNED-IN"))
-
-    
-    # request.session.update({"session_cookie":{"signed":False}})
-    # print(request.session.get("session_cookie"))
-    # print(request.headers)
-    # return templates.TemplateResponse("home.html", {"request": request, "title": "歡迎光臨，請輸入帳號密碼", "status": "登入系統"})
+    request.session.update({"ACCOUNT":None}) # reset the account info everytime when redirecting to /
     return templates.TemplateResponse(name = "home.html", context = {"request": request, "title": "歡迎光臨，請輸入帳號密碼", "status": "登入系統", "bonus":"計算正整數的平方"})
 
 @app.get('/member')
 def get_member(request: Request):
-    # request.session.update({"SIGNED-IN": False})
+    account = request.session.get('ACCOUNT')
+    print(account)
     # check session SIGNED-IN
-    print("member: check request.session")
-    print(request.session.get("SIGNED-IN"))
     if request.session.get("SIGNED-IN") is True:
-        return templates.TemplateResponse("member.html", {"request": request, "title": "歡迎光臨，這是會員頁", "status": "恭喜您，成功登入系統"})
+        return templates.TemplateResponse("member.html", {"request": request, "title": "歡迎光臨"+account+"，這是會員頁", "status": "恭喜您，成功登入系統"})
     else:
         return RedirectResponse('/',status_code=303)   
 
-
 @app.get('/error')
 def get_error(request: Request, message):
-    print("error:印出request.session")
-    print(request.session.get("SIGNED-IN"))
-    return templates.TemplateResponse("error.html", { "request":request, "title":"失敗頁面", "status":message})
+    request.session.update({"SIGNED-IN":False})
+    if message == "no_such_account":
+        account = request.session.get("ACCOUNT")
+        status = "不存在叫 "+account+" 的帳號"
+    else:
+        status = "密碼輸入錯誤"
+    
+    return templates.TemplateResponse("error.html", 
+                                      {"request":request, 
+                                       "title":"失敗頁面", 
+                                       "status":status})
 
 @app.get('/signout')
 def get_signout(request: Request):
-    # set signed-in state to false
-    print("signout:印出request.session")
-    print(request.session.get("SIGNED-IN"))
+    # set signed-in state to false, then redirect to /
     request.session.update({"SIGNED-IN":False})
     return RedirectResponse('/',status_code=303)  
-    # return templates.TemplateResponse("signout.html", {"request":request, "title":"失敗頁面", "status":"帳號、或密碼輸入錯誤"})
 
 @app.post('/signin')
 def verification(request: Request, account: str = Form("account"), password: str = Form("password")):
-    # print("帳號: ",account)
-    # print("密碼: ",password)
-    print("verification:印出request.session")
-    print(request.session.get("SIGNED-IN"))
 
+    # check submitted nputs with default account/password
     if account == database[0].account and password == database[0].password:
         request.session.update({"SIGNED-IN": True})
-        # redirect_url = request.url_for('member') 
-        return RedirectResponse('/member',status_code=303)   
+        request.session.update({"ACCOUNT": account})
+        # response = RedirectResponse('/member', status_code=303)
+        # response.set_cookie(key="account", value=account)
+        # return response
+        return RedirectResponse('/member', status_code=303)   
     elif account == "test" and password != "test":
         request.session.update({"SIGNED-IN": False})
         return RedirectResponse(
-            url = "/error"+ "?message=密碼輸入錯誤",
+            url = "/error"+ "?message=validation_fail",
             status_code=303
         )
     else:
         request.session.update({"SIGNED-IN": False})
-        # redirect_url = request.url_for('error') 
-        # return RedirectResponse('/error',status_code=303) 
+        request.session.update({"ACCOUNT": account})
         return RedirectResponse(
-            url = "/error"+ "?message=帳號不存在",
+            url = "/error"+ "?message=no_such_account",
             status_code=303
         )
-        
+
+# bonus
 @app.get('/square/{int_input}')
 def cal_square(request:Request, int_input: int):
-    print("print input")
-    print(type(int_input))
-    
+    # print("print input")
+    # print(type(int_input))
     return templates.TemplateResponse("error.html", { "request":request, "title":"正整數平方計算結果", "status":int_input**2})
 
 
